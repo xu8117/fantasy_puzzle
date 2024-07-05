2f95f63a5f884727c5caadef8fb59ac9007a368e
xu8117 <xu8117@gmail.com>
2024-07-05 23:50:26 +0800

    feat: init

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function Page() {
+    return (
+        <div>
+            <p>Dashboard Page</p>
+            <Link href="/">To Home</Link>
+        </div>
+
+
+    );
+}
